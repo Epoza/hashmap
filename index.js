7af0cb129ec76f65a2cac815d5af5f1905c35ed6
Epoza <86025026+Epoza@users.noreply.github.com>
2024-05-13 22:05:05 -0400

    add get, set, has, remove, length functions

--- index.js
+++ index.js
@@ -1,49 +1,123 @@
-function hashmap() {
-    function hash(key) {
-        let hashCode = 0;
-           
-        const primeNumber = 31;
-        for (let i = 0; i < key.length; i++) {
-          hashCode = primeNumber * hashCode + key.charCodeAt(i);
-        }
-     
-        return hashCode;
+const hashmap = () => {
+  const map = {
+      size: 16,
+      buckets: new Array(16).fill(null).map(() => [])
+  };
+
+  const hash = (key) => {
+      let hashCode = 0;
+      const primeNumber = 31;
+      for (let i = 0; i < key.length; i++) {
+          hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % map.size;
+      }
+      return hashCode;
+  }; 
+
+  const set = (key, value) => {
+      const index = hash(key);
+      const bucket = map.buckets[index];
+      if (index < 0 || index >= map.buckets.length) {
+          throw new Error("Trying to access index out of bound");
+      }
+      // Check if key already exists in the bucket
+      for (let i = 0; i < bucket.length; i++) {
+          if (bucket[i].key === key) {
+              // If key exists, update the value
+              bucket[i].value = value;
+              return;
+          }
+      }
+      // If key doesn't exist, push a new entry
+      bucket.push({ key, value });
+  };
+
+  const get = (key) => {
+    const index = hash(key);
+    const bucket = map.buckets[index];
+    if (index < 0 || index >= map.buckets.length) {
+      throw new Error("Trying to access index out of bound");
+    }
+    // Checks if key matches and existing entry
+    for(let i = 0; i < bucket.length; i++){
+      if(bucket[i].key === key){
+        // Return the value associated with that key
+        return bucket[i].value
       } 
+    }
+    return null
+  }
 
-      function set(key, value){
+  const has = (key) => {
+    const index = hash(key);
+    const bucket = map.buckets[index];
+    if (index < 0 || index >= map.buckets.length) {
+      throw new Error("Trying to access index out of bound");
+    }
+    // Checks if key matches and existing entry
+    for(let i = 0; i < bucket.length; i++){
+      if(bucket[i].key === key){
+        // Return the value associated with that key
+        return true
+      } 
+    }
+    return false
+  }
 
-      }
+  const remove = (key) => {
+    const index = hash(key);
+    const bucket = map.buckets[index];
+    if (index < 0 || index >= map.buckets.length) {
+      throw new Error("Trying to access index out of bound");
+    }
+    // Checks if key matches and existing entry
+    for(let i = 0; i < bucket.length; i++){
+      if(bucket[i].key === key){
+        // Remove the entry with that key
+        bucket.splice(i, 1); // Remove 1 element starting at index i
+        return true
+      } 
+    }
+    return false
+  }
 
-      function get(key) {
+const length = () => {
+    let count = 0;
+    for(let i = 0; i < map.buckets.length; i++){
+      count += map.buckets[i].length
+    }
+    return count
+  }
 
-      }
+  // function clear() {
 
-      function has(key){
+  // }
 
-      }
+  // function keys() {
 
-      function remove(key){
+  // }
 
-      }
+  // function values() {
 
-      function length() {
+  // }
 
-      }
+  // function entries() {
+  // }
 
-      function clear() {
+  return { 
+    hash,
+    set,
+    get,
+    has,
+    remove,
+    length
+   };
+};
 
-      }
-
-      function keys() {
-
-      }
-
-      function values() {
-
-      }
-
-      function entries() {
-        
-      }
-     
-}
+const myMap = hashmap();
+myMap.set("jeff", "hello")
+myMap.set("bob", "My name is Bob")
+myMap.set("chris", "My name is chris")
+console.log(myMap.get("jeff"));
+console.log(myMap.has("jeff"));
+console.log(myMap.length());
+console.log(myMap);
